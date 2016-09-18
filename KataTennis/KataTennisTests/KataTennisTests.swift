707971e232b333a948e445721e19d2743b1fa816
Kim SAVAROCHE <kim.savaroche@hins.fr>
2016-09-18 08:36:29 +0200

    init

---
+++ KataTennis/KataTennisTests/KataTennisTests.swift
@@ -0,0 +1,38 @@
+//
+//  KataTennisTests.swift
+//  KataTennisTests
+//
+//  Created by Kim SAVAROCHE on 18/09/16.
+//  Copyright © 2016 Kim SAVAROCHE. All rights reserved.
+//
+
+import XCTest
+@testable import KataTennis
+
+class KataTennisTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        XCTAssertEqual(2, 2);
+    }
+    func testExample2() {
+        XCTAssertEqual(2, 3);
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
